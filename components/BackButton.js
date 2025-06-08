03e819fb8cbd3ffa4f6ba89804bc7563df54568e
Dmx <digenmore4@gmail.com>
2025-06-08 12:31:02 +0530

    ⚒️feat: enhance UI components; adjust BackButton opacity, import BackButton in AboutScreen, and add navigation to About in ProfileScreen

--- components/BackButton.js
+++ components/BackButton.js
@@ -21,7 +21,7 @@ const BackButton = ({ onPress, style }) => {
     <TouchableOpacity
       style={[
         tw`absolute top-11 left-5 z-10 p-2 rounded-full`,
-        { backgroundColor: colors.primary + "20" },
+        { backgroundColor: colors.primary + "20", opacity: 0.8 },
         style,
       ]}
       onPress={handlePress}
